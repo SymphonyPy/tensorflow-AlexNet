b953db52c2c1f091e793f125085bf5b268baa8ec
SymPy <568603742@qq.com>
2018-02-22 23:27:03 +0800

    little changes in detail

--- Datagenerator.py
+++ Datagenerator.py
@@ -1,9 +1,11 @@
-import numpy as np 
+import numpy as np
 import cv2
 
+
 class ImageDataGenerator:
-    def __init__(self, class_list, n_class, batch_size = 1, flip = True, shuffle = False, mean = np.array([104., 117., 124.]), scale_size = (227,227)):
-        #initial params
+    def __init__(self, class_list, n_class, batch_size=1, flip=True, shuffle=False, mean=np.array([104., 117., 124.]),
+                 scale_size=(227, 227)):
+        # initial params
         self.horizontal = flip
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -16,17 +18,20 @@ class ImageDataGenerator:
         if shuffle:
             self.shuffle_data()
 
-    def read_class_list(self,class_list):
+    def read_class_list(self, class_list):
         with open(class_list) as f:
             lines = f.readlines()
             self.images = []
             self.labels = []
             for line in lines:
                 items = line.split()
-                self.images.append(items[0])
-                self.labels.append(items[1])
+                item = items[0].split("#")[0]
+                label = items[1]
+                print(item, label)
+                self.images.append(item)
+                self.labels.append(label)
             self.data_size = len(self.labels)
-    
+
     def shuffle_data(self):
         images = self.images.copy()
         labels = self.labels.copy()
@@ -41,27 +46,27 @@ class ImageDataGenerator:
         self.pointer = 0
         if self.shuffle:
             self.shuffle_data()
-    
+
     def getNext_batch(self):
-        paths = self.images[self.pointer:self.pointer+self.batch_size]
-        labels = self.labels[self.pointer:self.pointer+self.batch_size]
+        paths = self.images[self.pointer:self.pointer + self.batch_size]
+        labels = self.labels[self.pointer:self.pointer + self.batch_size]
         self.pointer += self.batch_size
-        
-        images = np.ndarray([self.batch_size,self.scale_size[0],self.scale_size[1],3])
+
+        images = np.ndarray([self.batch_size, self.scale_size[0], self.scale_size[1], 3])
         for i in range(len(paths)):
             image = cv2.imread(paths[i])
-            #print ('file name is {}'.format(paths[i]))
-            #cv2.imshow(paths[i],image)
-            #cv2.waitKey(0)
-            if self.horizontal and np.random.random()<0.5:
-                image = cv2.flip(image,1)
-            image = cv2.resize(image,(self.scale_size[0],self.scale_size[1]))
+            # print ('file name is {}'.format(paths[i]))
+            # cv2.imshow(paths[i],image)
+            # cv2.waitKey(0)
+            if self.horizontal and np.random.random() < 0.5:
+                image = cv2.flip(image, 1)
+            image = cv2.resize(image, (self.scale_size[0], self.scale_size[1]))
             image = image.astype(np.float32)
 
             image -= self.mean
             images[i] = image
 
-        one_hot_labels = np.zeros((self.batch_size,self.n_class))
+        one_hot_labels = np.zeros((self.batch_size, self.n_class))
         for i in range(len(labels)):
             one_hot_labels[i][int(labels[i])] = 1
-        return images,one_hot_labels
+        return images, one_hot_labels
